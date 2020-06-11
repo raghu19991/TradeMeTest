5f05378ee4610c8ac61a63eb07e66cdacdc2e6b8
raghu19991 <raghu19991@gmail.com>
2020-06-11 16:16:48 +1200

    Description of tests

--- tests/TestFile.py
+++ tests/TestFile.py
@@ -9,14 +9,14 @@ class MyTestCase(unittest.TestCase):
        cls.driver.get("https://www.tmsandbox.co.nz/")
        cls.get_All_UsedCar_Brands(cls)
 
+    #Method to get list of all visible cars in the Used Cars Page
     def get_All_UsedCar_Brands(self):
         page.MainPage(self.driver).go_to_motors()
         page.MotorsPage(self.driver).go_to_UsedCars()
         used_cars=page.UsedCarsPage(self.driver)
         all_UsedCars=used_cars.get_displayed_car_brands()
-        #available_cars=used_cars.available_cars()
-        #print('Used brands of Cars available in the marketplace are:', *available_cars, sep=' , ')
 
+    #test to get count of available cars i.unique cars that are > 0 in the page
     def test_Available_UsedCarBrands(self):
         list1=page.UsedCarsPage.available_cars(self)
         length=len(list1)
@@ -25,11 +25,13 @@ class MyTestCase(unittest.TestCase):
         print('    They are:-', *list1, sep=' ')
         print()
 
+    #test to check if brand Kia is available
     def test_kia_exists(self):
         list1=page.UsedCarsPage.available_cars(self)
         self.assertTrue('Kia' in list1)
         print(self._testMethodName+" --> pass\n")
 
+    #test to check if that brand Hispano Suiza shouldnt be available
     def test_HispanoSuiza_doesnt_exist(self):
         list1=page.UsedCarsPage.available_cars(self)
         self.assertTrue("Hispano Suiza" not in list1)
